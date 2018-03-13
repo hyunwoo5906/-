137aacb8aed028b28cdecf135cff10d6d85483a4
hyunwoo5906 <33214649+hyunwoo5906@users.noreply.github.com>
2018-03-13 20:58:58 +0900

    Add files via upload

---
+++ binaryTree/Example1.java
@@ -0,0 +1,60 @@
+package net.skhu.binaryTree;
+//파라미터 값 value가 이진트리에 들어있는지 여부를 boolean으로 리턴하는 contains메소드를 구현
+import java.util.Random;
+
+public class Example1 {
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        public void add(int value) {
+            if (value < this.value) {
+                if (left == null) left = new Node(value);
+                else left.add(value);
+            } else if (value > this.value) {
+                if (right == null) right = new Node(value);
+                else right.add(value);
+            }
+        }
+
+        public void print() {
+            if (left != null) left.print();
+            System.out.printf("%d ", value);
+            if (right != null) right.print();
+        }
+
+        public boolean contains(int value) {
+        	if(value<this.value) 
+        		return left!=null && left.contains(value);
+        	else if(value>this.value)
+        		return right!=null && right.contains(value);
+        	return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Node root = new Node(random.nextInt(20));
+        for (int i = 0; i < 15; ++i)
+            root.add(random.nextInt(20));
+
+        root.print();
+        System.out.println();
+
+        for (int i = 0; i < 20; ++i) {
+            int value = random.nextInt(20);
+            System.out.printf("%d %s\n", value, root.contains(value));
+        }
+
+    }
+
+}
+
