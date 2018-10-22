74403cf8265ab030fab7a222c60a4989db8cbf89
hyunwoo5906 <33214649+hyunwoo5906@users.noreply.github.com>
2018-10-22 18:55:27 +0900

    백준 11718

---
+++ baekjoon/Baekjoon11718.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon11718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextLine()) {
+			String text=input.nextLine();
+			
+			System.out.println(text);
+		}
+	}
+
+}
