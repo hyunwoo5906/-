f86cbc9484f9746ac6ed28274094ec3c7fe7dc58
hyunwoo5906 <33214649+hyunwoo5906@users.noreply.github.com>
2018-10-22 18:35:17 +0900

    백준 1000 입력문제

---
+++ baekjoon/Baekjoon1000.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input =new Scanner(System.in);
+		
+		int a =input.nextInt();
+		int b =input.nextInt();
+		
+		System.out.println(a+b);
+	}
+}
